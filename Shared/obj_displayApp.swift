c7b233aa26b0755e306819402e86712820c67494
Ingmar Drewing <ingmar@drewing.de>
2020-12-23 18:31:23 +0100

    Initial Commit

---
+++ Shared/obj_displayApp.swift
@@ -0,0 +1,17 @@
+//
+//  obj_displayApp.swift
+//  Shared
+//
+//  Created by private on 23.12.20.
+//
+
+import SwiftUI
+
+@main
+struct obj_displayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

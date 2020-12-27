8f84138d6a0686422f52f38c5d186a95849939f5
Ingmar Drewing <ingmar@drewing.de>
2020-12-27 12:03:31 +0100

    Add code and assets

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import SceneKit
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    var scene = SCNScene(named: "TestScene.scn")
+
+    var cameraNode: SCNNode? {
+            scene?.rootNode.childNode(withName: "camera", recursively: false)
+    }
+    var light: SCNNode? {
+            scene?.rootNode.childNode(withName: "light", recursively: false)
+    }
+    
+    func up () {
+        SCNTransaction.animationDuration = 1
+
+        SCNTransaction.begin()
+        light?.position.y = 0.5
+        SCNTransaction.commit()
+    }
+    
+    func down () {
+        SCNTransaction.animationDuration = 1
+        SCNTransaction.begin()
+        light?.position.y = -0.5
+        SCNTransaction.commit()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            SceneView(
+                    scene: scene,
+                    pointOfView: cameraNode,
+                    options: [.allowsCameraControl]
+            ).onReceive(timer){ _ in
+                if (self.light?.position.y == 0.5 ) {
+                    self.down()
+                }
+                else {
+                    self.up()
+                }
+            }
+
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
